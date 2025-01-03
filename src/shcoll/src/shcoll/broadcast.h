bfbf14deeedf46c85e6c77d5a560a0142387d4a8
michael-beebe <michael-beebe@users.noreply.github.com>
2025-01-02 21:56:51 -0600

    set up shcoll headers to support both typed and sized versions of the collectives

--- src/shcoll/src/shcoll/broadcast.h
+++ src/shcoll/src/shcoll/broadcast.h
@@ -1,3 +1,21 @@
+/**
+ * @file broadcast.h
+ * @brief Header file for broadcast collective operations
+ *
+ * This header declares the interfaces for broadcast collective operations
+ * using different algorithms and synchronization methods:
+ * - Linear
+ * - Complete tree 
+ * - Binomial tree
+ * - k-nomial tree
+ * - Scatter-collect
+ *
+ * Some algorithms have variants using different synchronization:
+ * - Barrier-based
+ * - Signal-based
+ * - Counter-based
+ */
+
 #ifndef _SHCOLL_BROADCAST_H
 #define _SHCOLL_BROADCAST_H 1
 
@@ -6,43 +24,84 @@
 void shcoll_set_broadcast_tree_degree(int tree_degree);
 void shcoll_set_broadcast_knomial_tree_radix_barrier(int tree_radix);
 
-#define SHCOLL_BROADCAST_DECLARATION(_algo, _type, _typename)                  \
-  int shcoll_##_typename##_broadcast_##_algo(                                  \
+/**
+ * @brief Macro to declare type-specific broadcast implementation
+ *
+ * @param _algo Algorithm name
+ * @param _type Data type
+ * @param _typename Type name string
+ */
+#define SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, _type, _typename)             \
+  int shcoll_##_typename##_broadcast_##_algo(                                   \
       shmem_team_t team, _type *dest, const _type *source, size_t nelems);
 
-/* Define all supported data types */
-#define DECLARE_FOR_ALL_TYPES_BROADCAST(_algo)                                 \
-  SHCOLL_BROADCAST_DECLARATION(_algo, float, float)                            \
-  SHCOLL_BROADCAST_DECLARATION(_algo, double, double)                          \
-  SHCOLL_BROADCAST_DECLARATION(_algo, long double, longdouble)                 \
-  SHCOLL_BROADCAST_DECLARATION(_algo, char, char)                              \
-  SHCOLL_BROADCAST_DECLARATION(_algo, signed char, schar)                      \
-  SHCOLL_BROADCAST_DECLARATION(_algo, short, short)                            \
-  SHCOLL_BROADCAST_DECLARATION(_algo, int, int)                                \
-  SHCOLL_BROADCAST_DECLARATION(_algo, long, long)                              \
-  SHCOLL_BROADCAST_DECLARATION(_algo, long long, longlong)                     \
-  SHCOLL_BROADCAST_DECLARATION(_algo, unsigned char, uchar)                    \
-  SHCOLL_BROADCAST_DECLARATION(_algo, unsigned short, ushort)                  \
-  SHCOLL_BROADCAST_DECLARATION(_algo, unsigned int, uint)                      \
-  SHCOLL_BROADCAST_DECLARATION(_algo, unsigned long, ulong)                    \
-  SHCOLL_BROADCAST_DECLARATION(_algo, unsigned long long, ulonglong)           \
-  SHCOLL_BROADCAST_DECLARATION(_algo, int8_t, int8)                            \
-  SHCOLL_BROADCAST_DECLARATION(_algo, int16_t, int16)                          \
-  SHCOLL_BROADCAST_DECLARATION(_algo, int32_t, int32)                          \
-  SHCOLL_BROADCAST_DECLARATION(_algo, int64_t, int64)                          \
-  SHCOLL_BROADCAST_DECLARATION(_algo, uint8_t, uint8)                          \
-  SHCOLL_BROADCAST_DECLARATION(_algo, uint16_t, uint16)                        \
-  SHCOLL_BROADCAST_DECLARATION(_algo, uint32_t, uint32)                        \
-  SHCOLL_BROADCAST_DECLARATION(_algo, uint64_t, uint64)                        \
-  SHCOLL_BROADCAST_DECLARATION(_algo, size_t, size)                            \
-  SHCOLL_BROADCAST_DECLARATION(_algo, ptrdiff_t, ptrdiff)
+/**
+ * @brief Macro to declare broadcast implementations for all supported types
+ *
+ * @param _algo Algorithm name to generate declarations for
+ */
+#define DECLARE_BROADCAST_TYPES(_algo)                                          \
+  SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, float, float)                       \
+  SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, double, double)                     \
+  SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, long double, longdouble)            \
+  SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, char, char)                         \
+  SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, signed char, schar)                 \
+  SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, short, short)                       \
+  SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, int, int)                           \
+  SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, long, long)                         \
+  SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, long long, longlong)                \
+  SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, unsigned char, uchar)               \
+  SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, unsigned short, ushort)             \
+  SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, unsigned int, uint)                 \
+  SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, unsigned long, ulong)               \
+  SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, unsigned long long, ulonglong)      \
+  SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, int8_t, int8)                       \
+  SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, int16_t, int16)                     \
+  SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, int32_t, int32)                     \
+  SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, int64_t, int64)                     \
+  SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, uint8_t, uint8)                     \
+  SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, uint16_t, uint16)                   \
+  SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, uint32_t, uint32)                   \
+  SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, uint64_t, uint64)                   \
+  SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, size_t, size)                       \
+  SHCOLL_TYPED_BROADCAST_DECLARATION(_algo, ptrdiff_t, ptrdiff)
 
-/* Declare for all algorithms */
-DECLARE_FOR_ALL_TYPES_BROADCAST(linear)
-DECLARE_FOR_ALL_TYPES_BROADCAST(complete_tree)
-DECLARE_FOR_ALL_TYPES_BROADCAST(binomial_tree)
-DECLARE_FOR_ALL_TYPES_BROADCAST(knomial_tree)
-DECLARE_FOR_ALL_TYPES_BROADCAST(knomial_tree_signal)
-DECLARE_FOR_ALL_TYPES_BROADCAST(scatter_collect)
+/* Declare all algorithm variants */
+DECLARE_BROADCAST_TYPES(linear)
+DECLARE_BROADCAST_TYPES(complete_tree)
+DECLARE_BROADCAST_TYPES(binomial_tree)
+DECLARE_BROADCAST_TYPES(knomial_tree)
+DECLARE_BROADCAST_TYPES(knomial_tree_signal)
+DECLARE_BROADCAST_TYPES(scatter_collect)
+
+/**
+ * @brief Macro to declare sized broadcast implementations
+ *
+ * @param _algo Algorithm name
+ * @param _size Size in bits
+ */
+#define SHCOLL_SIZED_BROADCAST_DECLARATION(_algo, _size)                        \
+  void shcoll_broadcast##_size##_##_algo(                                      \
+      void *dest, const void *source, size_t nelems, int PE_root,              \
+      int PE_start, int logPE_stride, int PE_size, long *pSync);
+
+/* Declare sized variants for each algorithm */
+SHCOLL_SIZED_BROADCAST_DECLARATION(linear, 32)
+SHCOLL_SIZED_BROADCAST_DECLARATION(linear, 64)
+
+SHCOLL_SIZED_BROADCAST_DECLARATION(complete_tree, 32)
+SHCOLL_SIZED_BROADCAST_DECLARATION(complete_tree, 64)
+
+SHCOLL_SIZED_BROADCAST_DECLARATION(binomial_tree, 32)
+SHCOLL_SIZED_BROADCAST_DECLARATION(binomial_tree, 64)
+
+SHCOLL_SIZED_BROADCAST_DECLARATION(knomial_tree, 32)
+SHCOLL_SIZED_BROADCAST_DECLARATION(knomial_tree, 64)
+
+SHCOLL_SIZED_BROADCAST_DECLARATION(knomial_tree_signal, 32)
+SHCOLL_SIZED_BROADCAST_DECLARATION(knomial_tree_signal, 64)
+
+SHCOLL_SIZED_BROADCAST_DECLARATION(scatter_collect, 32)
+SHCOLL_SIZED_BROADCAST_DECLARATION(scatter_collect, 64)
 
 #endif /* ! _SHCOLL_BROADCAST_H */
--- src/shcoll/src/shcoll/fcollect.h
+++ src/shcoll/src/shcoll/fcollect.h
@@ -1,111 +1,123 @@
-#ifndef _SCHOLL_FCOLLECT_H
-#define _SCHOLL_FCOLLECT_H 1
+/**
+ * @file fcollect.h
+ * @brief Header file for fixed-size collect collective operations
+ *
+ * This header declares the interfaces for fixed-size collect collective
+ * operations using different algorithms and synchronization methods:
+ * - Linear
+ * - All linear
+ * - Recursive doubling
+ * - Ring
+ * - Bruck
+ * - Neighbor exchange
+ *
+ * Some algorithms have variants using different synchronization:
+ * - Signal-based
+ */
+
+#ifndef _SHCOLL_FCOLLECT_H
+#define _SHCOLL_FCOLLECT_H 1
 
 #include <shmem/teams.h>
 
-#define SHCOLL_FCOLLECT_DECLARATION(_algo, _type, _typename)                    \
-  int shcoll_##_typename##_fcollect_##_algo(                                    \
+/**
+ * @brief Macro to declare type-specific fcollect implementation
+ *
+ * @param _algo Algorithm name
+ * @param _type Data type
+ * @param _typename Type name string
+ */
+#define SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, _type, _typename)             \
+  int shcoll_##_typename##_fcollect_##_algo(                                   \
       shmem_team_t team, _type *dest, const _type *source, size_t nelems);
 
-/* Define all supported data types */
-#define DECLARE_FOR_ALL_TYPES_FCOLLECT(_algo)                                   \
-  SHCOLL_FCOLLECT_DECLARATION(_algo, float, float)                              \
-  SHCOLL_FCOLLECT_DECLARATION(_algo, double, double)                            \
-  SHCOLL_FCOLLECT_DECLARATION(_algo, long double, longdouble)                   \
-  SHCOLL_FCOLLECT_DECLARATION(_algo, char, char)                                \
-  SHCOLL_FCOLLECT_DECLARATION(_algo, signed char, schar)                        \
-  SHCOLL_FCOLLECT_DECLARATION(_algo, short, short)                              \
-  SHCOLL_FCOLLECT_DECLARATION(_algo, int, int)                                  \
-  SHCOLL_FCOLLECT_DECLARATION(_algo, long, long)                                \
-  SHCOLL_FCOLLECT_DECLARATION(_algo, long long, longlong)                       \
-  SHCOLL_FCOLLECT_DECLARATION(_algo, unsigned char, uchar)                      \
-  SHCOLL_FCOLLECT_DECLARATION(_algo, unsigned short, ushort)                    \
-  SHCOLL_FCOLLECT_DECLARATION(_algo, unsigned int, uint)                        \
-  SHCOLL_FCOLLECT_DECLARATION(_algo, unsigned long, ulong)                      \
-  SHCOLL_FCOLLECT_DECLARATION(_algo, unsigned long long, ulonglong)             \
-  SHCOLL_FCOLLECT_DECLARATION(_algo, int8_t, int8)                              \
-  SHCOLL_FCOLLECT_DECLARATION(_algo, int16_t, int16)                            \
-  SHCOLL_FCOLLECT_DECLARATION(_algo, int32_t, int32)                            \
-  SHCOLL_FCOLLECT_DECLARATION(_algo, int64_t, int64)                            \
-  SHCOLL_FCOLLECT_DECLARATION(_algo, uint8_t, uint8)                            \
-  SHCOLL_FCOLLECT_DECLARATION(_algo, uint16_t, uint16)                          \
-  SHCOLL_FCOLLECT_DECLARATION(_algo, uint32_t, uint32)                          \
-  SHCOLL_FCOLLECT_DECLARATION(_algo, uint64_t, uint64)                          \
-  SHCOLL_FCOLLECT_DECLARATION(_algo, size_t, size)                              \
-  SHCOLL_FCOLLECT_DECLARATION(_algo, ptrdiff_t, ptrdiff)
+/**
+ * @brief Macro to declare fcollect implementations for all supported types
+ *
+ * @param _algo Algorithm name to generate declarations for
+ */
+#define DECLARE_FCOLLECT_TYPES(_algo)                                          \
+  SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, float, float)                       \
+  SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, double, double)                     \
+  SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, long double, longdouble)            \
+  SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, char, char)                         \
+  SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, signed char, schar)                 \
+  SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, short, short)                       \
+  SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, int, int)                           \
+  SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, long, long)                         \
+  SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, long long, longlong)                \
+  SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, unsigned char, uchar)               \
+  SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, unsigned short, ushort)             \
+  SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, unsigned int, uint)                 \
+  SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, unsigned long, ulong)               \
+  SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, unsigned long long, ulonglong)      \
+  SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, int8_t, int8)                       \
+  SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, int16_t, int16)                     \
+  SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, int32_t, int32)                     \
+  SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, int64_t, int64)                     \
+  SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, uint8_t, uint8)                     \
+  SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, uint16_t, uint16)                   \
+  SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, uint32_t, uint32)                   \
+  SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, uint64_t, uint64)                   \
+  SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, size_t, size)                       \
+  SHCOLL_TYPED_FCOLLECT_DECLARATION(_algo, ptrdiff_t, ptrdiff)
 
-/* Declare for all algorithms */
-DECLARE_FOR_ALL_TYPES_FCOLLECT(linear)
-DECLARE_FOR_ALL_TYPES_FCOLLECT(all_linear)
-DECLARE_FOR_ALL_TYPES_FCOLLECT(all_linear1)
-DECLARE_FOR_ALL_TYPES_FCOLLECT(rec_dbl)
-DECLARE_FOR_ALL_TYPES_FCOLLECT(rec_dbl_signal)
-DECLARE_FOR_ALL_TYPES_FCOLLECT(ring)
-DECLARE_FOR_ALL_TYPES_FCOLLECT(bruck)
-DECLARE_FOR_ALL_TYPES_FCOLLECT(bruck_no_rotate)
-DECLARE_FOR_ALL_TYPES_FCOLLECT(bruck_signal)
-DECLARE_FOR_ALL_TYPES_FCOLLECT(bruck_inplace)
-DECLARE_FOR_ALL_TYPES_FCOLLECT(neighbor_exchange)
+/* Declare all algorithm variants */
+DECLARE_FCOLLECT_TYPES(linear)
+DECLARE_FCOLLECT_TYPES(all_linear)
+DECLARE_FCOLLECT_TYPES(all_linear1)
+DECLARE_FCOLLECT_TYPES(rec_dbl)
+DECLARE_FCOLLECT_TYPES(rec_dbl_signal)
+DECLARE_FCOLLECT_TYPES(ring)
+DECLARE_FCOLLECT_TYPES(bruck)
+DECLARE_FCOLLECT_TYPES(bruck_no_rotate)
+DECLARE_FCOLLECT_TYPES(bruck_signal)
+DECLARE_FCOLLECT_TYPES(bruck_inplace)
+DECLARE_FCOLLECT_TYPES(neighbor_exchange)
 
+/**
+ * @brief Macro to declare sized fcollect implementations
+ *
+ * @param _algo Algorithm name
+ * @param _size Size in bits
+ */
+#define SHCOLL_SIZED_FCOLLECT_DECLARATION(_algo, _size)                        \
+  void shcoll_fcollect##_size##_##_algo(                                       \
+      void *dest, const void *source, size_t nelems, int PE_start,             \
+      int logPE_stride, int PE_size, long *pSync);
 
-#endif /* ! _SCHOLL_FCOLLECT_H */
+/* Declare sized variants for each algorithm */
+SHCOLL_SIZED_FCOLLECT_DECLARATION(linear, 32)
+SHCOLL_SIZED_FCOLLECT_DECLARATION(linear, 64)
 
+SHCOLL_SIZED_FCOLLECT_DECLARATION(all_linear, 32)
+SHCOLL_SIZED_FCOLLECT_DECLARATION(all_linear, 64)
 
+SHCOLL_SIZED_FCOLLECT_DECLARATION(all_linear1, 32)
+SHCOLL_SIZED_FCOLLECT_DECLARATION(all_linear1, 64)
 
+SHCOLL_SIZED_FCOLLECT_DECLARATION(rec_dbl, 32)
+SHCOLL_SIZED_FCOLLECT_DECLARATION(rec_dbl, 64)
 
+SHCOLL_SIZED_FCOLLECT_DECLARATION(rec_dbl_signal, 32)
+SHCOLL_SIZED_FCOLLECT_DECLARATION(rec_dbl_signal, 64)
 
+SHCOLL_SIZED_FCOLLECT_DECLARATION(ring, 32)
+SHCOLL_SIZED_FCOLLECT_DECLARATION(ring, 64)
 
+SHCOLL_SIZED_FCOLLECT_DECLARATION(bruck, 32)
+SHCOLL_SIZED_FCOLLECT_DECLARATION(bruck, 64)
 
+SHCOLL_SIZED_FCOLLECT_DECLARATION(bruck_no_rotate, 32)
+SHCOLL_SIZED_FCOLLECT_DECLARATION(bruck_no_rotate, 64)
 
+SHCOLL_SIZED_FCOLLECT_DECLARATION(bruck_signal, 32)
+SHCOLL_SIZED_FCOLLECT_DECLARATION(bruck_signal, 64)
 
+SHCOLL_SIZED_FCOLLECT_DECLARATION(bruck_inplace, 32)
+SHCOLL_SIZED_FCOLLECT_DECLARATION(bruck_inplace, 64)
 
+SHCOLL_SIZED_FCOLLECT_DECLARATION(neighbor_exchange, 32)
+SHCOLL_SIZED_FCOLLECT_DECLARATION(neighbor_exchange, 64)
 
-
-
-
-
-
-
-
-
-
-
-// #ifndef _SHCOLL_FCOLLECT_H
-// #define _SHCOLL_FCOLLECT_H 1
-
-// #define SHCOLL_FCOLLECT_DECLARATION(_name, _size)                              \
-//   void shcoll_fcollect##_size##_##_name(                                       \
-//       void *dest, const void *source, size_t nelems, int PE_start,             \
-//       int logPE_stride, int PE_size, long *pSync);
-
-// SHCOLL_FCOLLECT_DECLARATION(linear, 32)
-// SHCOLL_FCOLLECT_DECLARATION(linear, 64)
-
-// SHCOLL_FCOLLECT_DECLARATION(all_linear, 32)
-// SHCOLL_FCOLLECT_DECLARATION(all_linear, 64)
-
-// SHCOLL_FCOLLECT_DECLARATION(all_linear1, 32)
-// SHCOLL_FCOLLECT_DECLARATION(all_linear1, 64)
-
-// SHCOLL_FCOLLECT_DECLARATION(rec_dbl, 32)
-// SHCOLL_FCOLLECT_DECLARATION(rec_dbl, 64)
-
-// SHCOLL_FCOLLECT_DECLARATION(ring, 32)
-// SHCOLL_FCOLLECT_DECLARATION(ring, 64)
-
-// SHCOLL_FCOLLECT_DECLARATION(bruck, 32)
-// SHCOLL_FCOLLECT_DECLARATION(bruck, 64)
-
-// SHCOLL_FCOLLECT_DECLARATION(bruck_no_rotate, 32)
-// SHCOLL_FCOLLECT_DECLARATION(bruck_no_rotate, 64)
-
-// SHCOLL_FCOLLECT_DECLARATION(bruck_signal, 32)
-// SHCOLL_FCOLLECT_DECLARATION(bruck_signal, 64)
-
-// SHCOLL_FCOLLECT_DECLARATION(bruck_inplace, 32)
-// SHCOLL_FCOLLECT_DECLARATION(bruck_inplace, 64)
-
-// SHCOLL_FCOLLECT_DECLARATION(neighbor_exchange, 32)
-// SHCOLL_FCOLLECT_DECLARATION(neighbor_exchange, 64)
-
-// #endif /* ! _SHCOLL_FCOLLECT_H */
+#endif /* ! _SHCOLL_FCOLLECT_H */
