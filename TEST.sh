b6b4576a5afab4b7935e81359602163cf1ad2c35
michael-beebe <michael-beebe@users.noreply.github.com>
2025-03-10 11:14:52 -0500

    updated TEST.sh script

--- TEST.sh
+++ TEST.sh
@@ -3,8 +3,8 @@
 # --- Check if the version is provided and valid
 VERSION="$1"
 if [ $# -eq 1 ]; then
-  if [ "$VERSION" != "1.4" ] && [ "$VERSION" != "1.5" ]; then
-    echo "Error: Version must be either 1.4 or 1.5"
+  if [ "$VERSION" != "sos1.4" ] && [ "$VERSION" != "sos1.5" ] && [ "$VERSION" != "vv1.5" ]; then
+    echo "Error: Version must be either sos1.4 or sos1.5 or vv1.5"
     exit 1
   fi
 fi
@@ -13,7 +13,7 @@ OSSS_DIR="$(pwd)"
 HLINE="---------------------------------------------------------"
 
 # --- Function to test version 1.4
-test_1_4() {
+test_sos_1_4() {
   cd ../tests-sos-1.4
   echo $HLINE
   echo "Running tests in $(pwd)"
@@ -22,8 +22,16 @@ test_1_4() {
 }
 
 # --- Function to test version 1.5  
-test_1_5() {
+test_sos_1_5() {
   cd ../tests-sos-1.5.2
+  echo $HLINE
+  echo "Running tests in $(pwd)"
+  ./TEST.sh
+  cd $OSSS_DIR
+}
+
+test_vv_1_5() {
+  cd ../shmemvv_1.5
   echo $HLINE
   echo "Running tests in $(pwd)"
   ./TEST.sh
@@ -33,10 +41,12 @@ test_1_5() {
 # --- Run the appropriate test(s)
 if [ $# -eq 0 ]; then
   # No version specified - run both
-  test_1_4
-  test_1_5
-elif [ "$VERSION" == "1.4" ]; then
-  test_1_4
-elif [ "$VERSION" == "1.5" ]; then
-  test_1_5
+  test_sos_1_4
+  test_sos_1_5
+elif [ "$VERSION" == "sos1.4" ]; then
+  test_sos_1_4
+elif [ "$VERSION" == "sos1.5" ]; then
+  test_sos_1_5
+elif [ "$VERSION" == "vv1.5" ]; then
+  test_vv_1_5
 fi
