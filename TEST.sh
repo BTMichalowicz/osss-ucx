1c0568a5d428e2184fa9acad27e94fd026fcf850
michael-beebe <michaelbeebe@protonmail.com>
2025-05-13 09:58:40 -0700

    Added option to TEST.sh script to run the shmemvv version 1.4 unit tests

--- TEST.sh
+++ TEST.sh
@@ -3,7 +3,7 @@
 # --- Check if the test is provided and valid
 TEST="$1"
 if [ $# -eq 1 ]; then
-  if [ "$TEST" != "sos1.4" ] && [ "$TEST" != "sos1.5" ] && [ "$TEST" != "vv1.5" ] && [ "$TEST" != "shmembench_1.4" ] && [ "$TEST" != "shmembench_1.5" ]; then
+  if [ "$TEST" != "sos1.4" ] && [ "$TEST" != "sos1.5" ] && [ "$TEST" != "vv1.4" ] && [ "$TEST" != "vv1.5" ] && [ "$TEST" != "shmembench_1.4" ] && [ "$TEST" != "shmembench_1.5" ]; then
     echo "Error: Version must be either sos1.4 or sos1.5 or vv1.5 or shmembench_1.4 or shmembench_1.5"
     exit 1
   fi
@@ -24,6 +24,15 @@ test_sos_1_4() {
 # --- Run SOS 1.5 tests
 test_sos_1_5() {
   cd ../tests-sos-1.5.2
+  echo $HLINE
+  echo "Running tests in $(pwd)"
+  ./TEST.sh
+  cd $OSSS_DIR
+}
+
+# --- Run SHMEMVV 1.4 tests
+test_vv_1_4() {
+  cd ../shmemvv_1.4
   echo $HLINE
   echo "Running tests in $(pwd)"
   ./TEST.sh
@@ -66,6 +75,8 @@ elif [ "$TEST" == "sos1.4" ]; then
   test_sos_1_4
 elif [ "$TEST" == "sos1.5" ]; then
   test_sos_1_5
+elif [ "$TEST" == "vv1.4" ]; then
+  test_vv_1_4
 elif [ "$TEST" == "vv1.5" ]; then
   test_vv_1_5
 elif [ "$TEST" == "shmembench_1.4" ]; then
