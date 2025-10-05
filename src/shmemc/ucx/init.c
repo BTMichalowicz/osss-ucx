729f1e59ae55101948efafba32e3c0652dabdeac
Ben Michalowicz <benjaminmichalowicz98@gmail.com>
2025-10-05 10:17:34 -0600

    added functionality for Active Messages, but snag on the callback for appropriate ciphertext.

--- src/shmemc/ucx/init.c
+++ src/shmemc/ucx/init.c
@@ -376,7 +376,11 @@ inline static void ucx_init_ready(void) {
 
   pm.features = UCP_FEATURE_RMA |   /* put/get */
                 UCP_FEATURE_AMO32 | /* 32-bit atomics */
-                UCP_FEATURE_AMO64;  /* 64-bit atomics */
+                UCP_FEATURE_AMO64;
+#if ENABLE_SHMEM_ENCRYPTION 
+                  /* 64-bit atomics */
+   pm.features |= UCP_FEATURE_AM;     /* Active messaging for put/get */
+#endif /* ENABLE_SHMEM_ENCRYPTION */
 
   pm.mt_workers_shared = (proc.td.osh_tl > SHMEM_THREAD_SINGLE);
 
